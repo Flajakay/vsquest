3b0566ae653c8cd6c6064ec8bd4d8c2479891ec5
Flajakay <33432218+Flajakay@users.noreply.github.com>
2025-10-26 11:17:21 +0100

    addjounralentry action, updated giveitemaction for itemizer integration

--- src/Systems/ActionUtil.cs
+++ src/Systems/ActionUtil.cs
@@ -50,26 +50,44 @@ namespace VsQuest
             recruit.WatchedAttributes.MarkPathDirty("guardedPlayerUid");
         }
 
-        public static void GiveItem(ICoreServerAPI sapi, QuestMessage message, IServerPlayer byPlayer, string[] args)
-        {
-            string code = args[0];
-            CollectibleObject item = sapi.World.GetItem(new AssetLocation(code));
-            if (item == null)
-            {
-                item = sapi.World.GetBlock(new AssetLocation(code));
-            }
-            if (item == null)
-            {
-                throw new QuestException(string.Format("Could not find item {0} for quest {1}!", code, message.questId));
-            }
-
-            var stack = new ItemStack(item, int.Parse(args[1]));
-            if (!byPlayer.InventoryManager.TryGiveItemstack(stack))
-            {
-                sapi.World.SpawnItemEntity(stack, byPlayer.Entity.ServerPos.XYZ);
-            }
-        }
-
+                public static void GiveItem(ICoreServerAPI sapi, QuestMessage message, IServerPlayer byPlayer, string[] args)
+                {
+                    if (args.Length < 2)
+                    {
+                        throw new QuestException("The 'giveitem' action requires at least 2 arguments: itemCode and amount.");
+                    }
+        
+                    string code = args[0];
+                    int amount = int.Parse(args[1]);
+        
+                    CollectibleObject item = sapi.World.GetItem(new AssetLocation(code));
+                    if (item == null)
+                    {
+                        item = sapi.World.GetBlock(new AssetLocation(code));
+                    }
+                    if (item == null)
+                    {
+                        throw new QuestException(string.Format("Could not find item {0} for quest {1}!", code, message.questId));
+                    }
+        
+                    var stack = new ItemStack(item, amount);
+        
+                    // Itemizer integration
+                    if (args.Length > 2)
+                    {
+                        stack.Attributes.SetString("itemizerName", args[2]);
+                    }
+                    if (args.Length > 3)
+                    {
+                        string desc = string.Join(" ", args, 3, args.Length - 3);
+                        stack.Attributes.SetString("itemizerDesc", desc);
+                    }
+        
+                    if (!byPlayer.InventoryManager.TryGiveItemstack(stack))
+                    {
+                        sapi.World.SpawnItemEntity(stack, byPlayer.Entity.ServerPos.XYZ);
+                    }
+                }
         public static void CompleteQuest(ICoreServerAPI sapi, QuestMessage message, IServerPlayer byPlayer, string[] args)
         {
             var questSystem = sapi.ModLoader.GetModSystem<QuestSystem>();
---
+++ src/Systems/JournalAction.cs
@@ -0,0 +1,51 @@
+
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+using Vintagestory.GameContent;
+using System.Collections.Generic;
+
+namespace VsQuest
+{
+    public class JournalActionSystem : ModSystem
+    {
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            var questSystem = api.ModLoader.GetModSystem<QuestSystem>();
+            if (questSystem != null)
+            {
+                questSystem.ActionRegistry.Add("addjournalentry", AddJournalEntry);
+            }
+        }
+
+        private void AddJournalEntry(ICoreServerAPI sapi, QuestMessage message, IServerPlayer player, string[] args)
+        {
+            if (args.Length < 3)
+            {
+                throw new QuestException("The 'addjournalentry' action requires at least 3 arguments: loreCode, title, and at least one chapter.");
+            }
+
+            var modJournal = sapi.ModLoader.GetModSystem<ModJournal>();
+            if (modJournal == null)
+            {
+                throw new QuestException("ModJournal system not found. Make sure VSSurvivalMod is enabled.");
+            }
+
+            var loreCode = args[0];
+            var title = args[1];
+            var chapters = new List<JournalChapter>();
+            for (int i = 2; i < args.Length; i++)
+            {
+                chapters.Add(new JournalChapter() { Text = args[i] });
+            }
+
+            var journalEntry = new JournalEntry()
+            {
+                Title = title,
+                LoreCode = loreCode,
+                Chapters = chapters
+            };
+
+            modJournal.AddOrUpdateJournalEntry(player, journalEntry);
+        }
+    }
+}
